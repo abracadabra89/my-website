7740fa3085ac6fbb1abbeffbe4958831a773b8d4
Anya <anya.s.litvinova@gmail.com>
2020-03-18 18:02:36 -0400

    the base structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from './components/about'
+import Introduction from "./components/introduction";
 
-function App() {
+
+class App extends React.Component {
+  render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div id="colorlib-page">
+        <div id="container-wrap">
+           <div id="colorlib-main">
+  <Introduction></Introduction>
+					<About></About>
+          	</div>
+      	</div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
