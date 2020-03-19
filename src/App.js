0194b9d491305f6a158324dc57354d9f6c1d4b3b
Anya <anya.s.litvinova@gmail.com>
2020-03-19 09:15:38 -0400

    css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import About from "./components/about";
 import Introduction from "./components/introduction";
 import Sidebar from "./components/sidebar";
+import Timeline from "./components/timeline";
+
 
 
 class App extends React.Component {
@@ -14,6 +16,7 @@ class App extends React.Component {
           <div id="colorlib-main">
             <Introduction />
             <About />
+            <Timeline />
           </div>
         </div>
       </div>
---
+++ src/components/timeline.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Art <span>2017</span>
+                        </h2>
+                        <p>
+                          blah blah
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Startup <span>2018</span>
+                        </h2>
+                       blah blah
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Flatiron Bootcamp <span>fall 2019 - winter 2020</span>
+                        </h2>
+                        <p>
+                          blah blah
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
