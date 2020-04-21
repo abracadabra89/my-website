f8cb50b14951c50ec54d9384370b4204454138a9
Anya <anya.s.litvinova@gmail.com>
2020-04-21 16:42:07 -0400

    js

--- public/js/main.js
+++ public/js/main.js
@@ -243,13 +243,13 @@
         $("#sticky_item").trigger("sticky_kit:detach");
         $("#sticky_item").trigger("sticky_kit:unstick");
 
-        $("#sticky_item").stick_in_parent();
+       
       }
     });
 
     $(".sticky-parent").css("height", h);
 
-    $("#sticky_item").stick_in_parent();
+   
   };
 
   var owlCrouselFeatureSlide = function () {
