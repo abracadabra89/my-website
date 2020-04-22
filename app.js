f9e1f030b210beb78f1bb803e8f3ff777c8733af
Anya <anya.s.litvinova@gmail.com>
2020-04-22 13:08:40 -0400

    css

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-var express = require("express");
-var serveIndex = require("serve-index");
-var path = require("path");
-var serveStatic = require("serve-static");
-var app = express();
-var port = process.env.PORT || 3000;
-/**for files */
-app.use(serveStatic(path.join(__dirname, "public")));
-/**for directory */
-app.use("/", express.static("public"), serveIndex("public", { icons: true }));
-
-// Listen
-app.listen(port, function () {
-  console.log("listening on port:", +port);
-});
+const express = require("express");
+const http = require("http");
+const path = require("path");
+let app = express();
+app.use(express.static(path.join(__dirname, "build")));
+const port = process.env.PORT || "8080";
+app.set("port", port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
